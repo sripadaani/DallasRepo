3a38575dbddced7608919ced8a15c0ccc167837f
root <root@ip-10-10-1-157.ap-south-1.compute.internal>
2024-09-22 22:20:15 +0000

    change on num-finder-1.sh file

--- shell-scripts/num-finder-1.sh
+++ shell-scripts/num-finder-1.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-#
 
-# List of numbers
-numbers=("1" "2" "3" "4" "5" "6")
+# Input list of numbers
+numbers=("123" "-456" "789" "-1011" "234" "-3456")
 
-# Print only the numbers that start with '-'
-for i in "${numbers[@]}"; do
-  if [ $(( i % 2)) -eq 1 ]; then
-   if [ $i -eq 4 ]; then
-   continue 
-   echo "printing ODD numbers:" $number  
-   fi
-  fi 
+# Arrays to store positive and negative numbers
+positive_numbers=()
+negative_numbers=()
+
+# Loop through the numbers and separate them
+for number in "${numbers[@]}"; do
+  if [[ $number == -* ]]; then
+    negative_numbers+=("$number")
+  else
+    positive_numbers+=("$number")
+  fi
 done
+
+# Output the results
+echo "Positive Numbers: ${positive_numbers[@]}"
+echo "Negative Numbers: ${negative_numbers[@]}"
 
